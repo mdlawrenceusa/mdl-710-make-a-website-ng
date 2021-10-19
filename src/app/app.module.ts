c0172b78513317ed07805b39664e83d2b260a39b
mdlawrenceusa <mdlawrence@me.com>
2021-10-19 10:36:58 -0400

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Lesson1Component } from './lesson1/lesson1.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent,
+                  HelloComponent,
+                  Lesson1Component
+                
+                
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/scratch.ts
@@ -0,0 +1,1 @@
+import { Lesson1Component } from './lesson1/lesson1.component';
